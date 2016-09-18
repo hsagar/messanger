0d520fd683179f9739eb9995b3768a66afbb0722
hsagar <hadawale.sagar@gmail.com>
2016-09-17 20:05:01 -0500

    Added profiles and POST messages

--- src/main/java/code/sagar/resources/MessageResource.java
+++ src/main/java/code/sagar/resources/MessageResource.java
@@ -3,9 +3,7 @@ package code.sagar.resources;
 import code.sagar.model.Message;
 import code.sagar.service.MessageService;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -18,8 +16,22 @@ public class MessageResource {
     MessageService messageService = new MessageService();
 
     @GET
-    @Produces(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getMessages() {
         return messageService.getAllMessages();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Message addMessage(Message message) {
+        return messageService.addMessage(message);
+    }
+
+    @GET
+    @Path("/{messageId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Message getMessage(@PathParam("messageId") long id) {
+        return messageService.getMessage(id);
     }
 }
--- src/main/java/code/sagar/service/MessageService.java
+++ src/main/java/code/sagar/service/MessageService.java
@@ -1,21 +1,48 @@
 package code.sagar.service;
 
+import code.sagar.database.DatabaseClass;
 import code.sagar.model.Message;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Sagar
  */
 public class MessageService {
 
+    private Map<Long, Message> messages = DatabaseClass.getMessages();
+
+    public MessageService() {
+        messages.put(1L, new Message(1, "Hello World!!!", "Sagar"));
+        messages.put(2L, new Message(2, "Hello Jersey!!!", "Sagar"));
+    }
+
+
     public List<Message> getAllMessages() {
-        Message message1 = new Message(1L, "Hello World!!!", "Sagar");
-        Message message2 = new Message(2L, "Hello Jersey!!!", "Sagar");
-        List<Message> messageList = new ArrayList<>();
-        messageList.add(message1);
-        messageList.add(message2);
-        return messageList;
+        return new ArrayList<Message>(messages.values());
+    }
+
+    public Message getMessage(long id) {
+        return messages.get(id);
+    }
+
+    public Message addMessage(Message message) {
+        message.setId(messages.size() + 1);
+        messages.put(message.getId(), message);
+        return message;
+    }
+
+    public Message updateMessage(Message message) {
+        if (message.getId() <= 0) {
+            return null;
+        }
+        messages.put(message.getId(), message);
+        return message;
+    }
+
+    public Message removeMessage(long id) {
+        return messages.remove(id);
     }
 }
