2053e71f765c7e2f46430f72eb2d0a210becac5b
hsagar <hadawale.sagar@gmail.com>
2016-09-17 20:14:19 -0500

    Implemented PUT and DELETE

--- src/main/java/code/sagar/resources/MessageResource.java
+++ src/main/java/code/sagar/resources/MessageResource.java
@@ -34,4 +34,21 @@ public class MessageResource {
     public Message getMessage(@PathParam("messageId") long id) {
         return messageService.getMessage(id);
     }
+
+    @PUT
+    @Path("/{messageId}}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Message updateMessage(@PathParam("messageId") long id, Message message) {
+        message.setId(id);
+        return messageService.updateMessage(message);
+    }
+
+    @DELETE
+    @Path("/{messageId}}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public void deleteMessage(@PathParam("messageId") long id, Message message) {
+        message.setId(id);
+        messageService.removeMessage(id);
+    }
 }
